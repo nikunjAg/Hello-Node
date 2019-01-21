ef489c32e03f867d7742f98cd0ff3e6a62ae512d
nikunjAg <aggarwalnikunj1999@gmail.com>
2019-01-21 21:33:36 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+// To let the app know we want to add support for partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+// To set up the view-engine compulsory
+// it uses the default views/ directory to render the .hbs pages
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    fs.appendFileSync('logger.txt', `${new Date().toString()}: ${req.method}: ${req.url}\n`);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('Maintenance.hbs', {
+        pageTitle: 'Maintenance'
+    });
+});
+
+// We used a middleware to make express work in a bit different way
+// Normally express use GET or POST route for making request
+// But here use used the middleware and asked express to
+// use our public directory and respond to the request made to any of that.
+app.use(express.static(__dirname + '/public'));
+
+// we can use the registerHelper to make its use to pass the data at the time
+// of request made to the server
+// inside this helper we do sent in data that is common to many request
+// while rendering
+// Now we don't need to pass in the data at the time of rendering
+// the partials can automatically use it by its name
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home',
+        welcomeMessage: 'Welcome to our home page'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.sendFile('./public/help.html');
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        code: 404,
+        errorMessage: 'error handling request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('App is running of port 3000');
+});
